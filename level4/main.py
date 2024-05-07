5b017c931c8eb2ba12ad364617211b16fbe215b9
JacopoAoD <jacopo.silvestri01@gmail.com>
2024-05-07 14:30:48 +0200

    l4

--- level4/main.py
+++ level4/main.py
@@ -1,6 +1,7 @@
 import json
 from datetime import timedelta, datetime
 from dateutil import easter
+import math
 
 with open('data.json', 'r') as f:
     data = json.load(f)
@@ -52,13 +53,20 @@ def daysAvailable(start_date, end_date, birthday):
 
     for i in years:
         birthday = birthday.replace(year=i)
-        if start_date <= birthday <= end_date and birthday.weekday() != 5 and birthday.weekday() != 6 and birthday.strftime("%m-%d") not in holidaysItaly and birthday not in easter_monday_dates:
+        if start_date <= birthday <= end_date and birthday.weekday() != 5 and birthday.weekday() != 6:
             workdays_with_bd -= 1
             #holidays +=1 commented because we don't have to save the holidays for every developer in the output file
             #just need the "common" ones
 
     return totaldays, workdays, workdays_with_bd, weekend, holidays   
 
+def distribute_effort(effort_dev, effort_required):
+    #I know the effort days available for the devs and how much it is required.
+    effort_for_day = round(effort_required / effort_dev, 2) #effort_for_day is useful to know how much every day the developer should work to the project.
+    hours_for_day = effort_for_day * 8 #8 as I suppose that every work day lasts 8 hours
+    minutes = int((hours_for_day - int(hours_for_day)) * 60) 
+    days = math.ceil(effort_dev / 3 * effort_for_day)
+    return days, hours_for_day, minutes
 
 def main():
     for project in projects:
@@ -67,7 +75,7 @@ def main():
         totaldays = 0
         workdays = 0
         weekend = 0
-        holidays = 0
+        holidays = 0        
         start_date = datetime.strptime(project['since'], "%Y-%m-%d").date()
         end_date = datetime.strptime(project['until'], "%Y-%m-%d").date()    
         if end_date < start_date:
@@ -88,8 +96,17 @@ def main():
         availability["weekend_days"] = weekend
         availability["holidays"] = holidays    
         availability["feasibility"] = total_effort_days >= project["effort_days"]  
-        availabilities.append(availability)  
+        # ----- Everything prior to this point is part of level 3 -------
+        # Now I know the feasibility of every single project. If a project is doable, I want to distribute the effort between developers.
+        # With the "distribute_effort" function I calculate:
+        #   - how many hour for day every developer should work to that project in order to distribute the effort.
+        #   - in how many days is doable the project if every dev works all day to that project
+        if availability["feasibility"] == True:         
+            days, hours_for_day, minutes = distribute_effort(total_effort_days, project["effort_days"])
+            availability["daily_work_for_dev"] = f"{int(hours_for_day):02d} hours: {minutes:02d} minutes"
+            availability["doable_in"] = f"{days} days"    
 
+        availabilities.append(availability)  
 
     with open("output.json", "w") as json_file:
         json.dump({"availabilities": availabilities}, json_file, indent=4)
